d41ee4f960b025f4aad680519a08b6cc4481dbe6
David Raygoza <david_raygoza@hotmail.com>
2023-03-02 17:03:59 -0600

    Adding styles in a stylesheet

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
+  <div id="home">
   <p>Test template</p>
   <h3>Adding text</h3>
+</div>
   `,
   styleUrls: ['./home.component.css']
 })
