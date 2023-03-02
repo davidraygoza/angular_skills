db6699bea09921d3ff7a1b0ac535e182e89f57f8
David Raygoza <david_raygoza@hotmail.com>
2023-03-02 14:06:35 -0600

    Changing templateUrl to template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  template: `
+  <p>Test template</p>
+  <h3>Adding text</h3>
+  `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
